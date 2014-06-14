08a307e846a6513fca9b9ba6e245218047d285e8
erkan <info@erkanay.com>
2014-06-14 19:06:06 +0300

    Create upload.class.php

---
+++ upload.class.php
@@ -0,0 +1,39 @@
+<?php
+class Uploader{
+	public function photoUpload(){
+		$path = 'uploads/';
+		$file_ext   = array('jpg','png','gif','bmp');
+		$post_ext   = end(explode('.',$_FILES['photo']['name']));
+		$photo_name = $_FILES['photo']['name'];
+		$photo_type = $_FILES['photo']['type'];
+		$photo_size = $_FILES['photo']['size'];
+		$photo_tmp  = $_FILES['photo']['tmp_name'];
+		$photo_error= $_FILES['photo']['error'];
+		//move_uploaded_file($photo_tmp,"uploads/".$photo_name);
+		if((($photo_type == 'image/jpeg') || ($photo_type == 'image/gif')   ||
+		   ($photo_type == 'image/png') || ($photo_type == 'image/pjpeg')) &&
+		   ($photo_size < 2000000) && in_array($post_ext,$file_ext)){
+			if($photo_error > 0 ){
+				echo 'Error '.$photo_error;
+				exit;
+			}else{
+				echo $photo_name.' Uploaded !';
+			}
+			if(file_exists($path.$photo_name)){
+				echo 'There is '.$photo_name;
+			}else{
+				//new photo name and encryption
+				$new_name = explode('.',$photo_name);
+				$photo_name = 'erkan_'.md5($new_name[0]).'.'.$new_name[1];
+				if(move_uploaded_file($photo_tmp,$path.$photo_name)){
+					echo "<img src=$path.$photo_name />";
+				}
+			}
+		}
+		else{
+			echo 'The uploaded file has invalid rules';
+		}
+	}
+}	
+$upload = new Uploader();
+?>
